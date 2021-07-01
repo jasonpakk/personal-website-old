6093145cf2a0104e089e2055a5f226eb86aec223
JY2452 <jaeyoung.pak8@gmail.com>
2021-07-01 18:17:01 -0400

    contact section into react components

---
+++ src/components/Contact/ContactItems.js
@@ -0,0 +1,24 @@
+const ContactItems = [
+  {
+    name: 'Email',
+    link: 'mailto:jaeyoung.pak8@gmail.com',
+    icon: 'fas fa-envelope fa-5x',
+  },
+  {
+    name: 'Github',
+    link: 'https://github.com/JY2452',
+    icon: 'fab fa-github fa-5x',
+  },
+  {
+    name: 'Facebook',
+    link: 'https://www.facebook.com/jason.pak0',
+    icon: 'fab fa-facebook fa-5x',
+  },
+  {
+    name: 'Instagram',
+    link: 'https://www.instagram.com/jasonpakk/',
+    icon: 'fab fa-instagram fa-5x',
+  },
+];
+
+export default ContactItems;
