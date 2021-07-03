b7810ee63d73f1ea009fbce00ed814a41c034ee1
JY2452 <jaeyoung.pak8@gmail.com>
2021-07-03 03:20:33 -0400

    initial install of react particles + fixing scroll w navbar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+
+const particleParams = {
+  particles: {
+    number: {
+      value: 50,
+    },
+    size: {
+      value: 3,
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse',
+      },
+    },
+  },
+};
 
 const About = () => {
   return (
     <section data-index="0" id="about">
-      <h2>Hi, I&apos;m Jason.</h2>
-      <p>I am a student at Dartmouth College.</p>
+      <Particles className="particles" params={particleParams} />
+      <div id="aboutText">
+        <h2>Hi, I&apos;m Jason.</h2>
+        <p>I am a student at Dartmouth College.</p>
+      </div>
+
     </section>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,9 +18,7 @@ class NavBar extends Component {
 
   handleMenuClick = (event) => {
     const sectionName = event.target.getAttribute('data-page');
-    const section = document.getElementById(sectionName);
-    window.scroll({ top: (section.offsetTop - 90), behavior: 'smooth' });
-    // document.getElementById(sectionName).scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(sectionName).scrollIntoView({ behavior: 'smooth' });
     this.setState(() => ({ clicked: false }));
   }
 
