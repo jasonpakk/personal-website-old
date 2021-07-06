752de4737cc95f817b518e4f2ab1c7521a66ce8b
JY2452 <jaeyoung.pak8@gmail.com>
2021-07-06 13:37:17 -0400

    home/about section split

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
     <section data-index="0" id="home">
       <ParticleBackground />
       <div id="homeText">
-        <h2>Hi, I&apos;m Jason.</h2>
-        <p>I am a Computer Science Major at Dartmouth College.</p>
+        <h2>Jason Pak</h2>
+        <p>I am studying Computer Science @ Dartmouth College</p>
       </div>
 
     </section>
