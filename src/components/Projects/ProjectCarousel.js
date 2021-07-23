08030d1d3ad07a0ddc76213cdc9d7573c31fa843
JY2452 <jaeyoung.pak8@gmail.com>
2021-07-23 16:06:22 -0400

    project component added

--- src/components/Projects/ProjectCarousel.js
+++ src/components/Projects/ProjectCarousel.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import ProjectObject from './ProjectObject';
 
 const options = {
   infiniteLoop: true,
@@ -55,18 +56,7 @@ class ProjectCarousel extends Component {
           selectedItem={this.state.currentSlide}
           onChange={this.updateCurrentSlide}
         >
-          <div className="carouselItem" key="slide1">
-            <img
-              alt="hi1"
-              src="https://i0.wp.com/readysetpuppy.com/wp-content/uploads/2019/09/Corgi-at-home.jpg?fit=998%2C463&ssl=1&resize=1280%2C720"
-            />
-          </div>
-          <div className="carouselItem" key="slide2">
-            <img
-              alt="hi2"
-              src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/14112506/Pembroke-Welsh-Corgi-standing-outdoors-in-the-fall.jpg"
-            />
-          </div>
+          <ProjectObject name="corgi" />
         </Carousel>
 
         <button type="button" onClick={this.next}>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-import ProjectCarousel from './ProjectCarousel';
+// import ProjectCarousel from './ProjectCarousel';
+// import ProjectObject from './ProjectObject';
 
 const Projects = () => {
   return (
     <section data-index="4" id="projects">
       <h2>Projects</h2>
       <p>Currently Under Construction</p>
-      <ProjectCarousel />
     </section>
   );
+  /*
+  return (
+    <section data-index="4" id="projects">
+      <h2>Projects</h2>
+      <p>Currently Under Construction</p>
+      <ProjectObject name="something" />
+      <ProjectObject name="personal" />
+      <ProjectObject name="tse" />
+      <ProjectObject name="botthoven" />
+    </section>
+  );
+  */
 };
 
 export default Projects;
