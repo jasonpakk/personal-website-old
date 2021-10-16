c0b1ea1d1fc2278b26929b78f4cd4f57696367e2
JY2452 <jaeyoung.pak8@gmail.com>
2021-10-15 23:47:20 -0400

    import music files

--- src/components/Personal/FoodItems.js
+++ src/components/Personal/FoodItems.js
@@ -1,9 +1,9 @@
-import corn from '../../img/food/corn.jpg';
-import kbbq from '../../img/food/kbbq.jpg';
-import nacho from '../../img/food/nacho.jpg';
-import padthai from '../../img/food/padthai.jpg';
-import pbelly from '../../img/food/pbelly.jpg';
-import shrimp from '../../img/food/shrimp.jpg';
+import corn from '../../img/personal/corn.jpg';
+import kbbq from '../../img/personal/kbbq.jpg';
+import nacho from '../../img/personal/nacho.jpg';
+import padthai from '../../img/personal/padthai.jpg';
+import pbelly from '../../img/personal/pbelly.jpg';
+import shrimp from '../../img/personal/shrimp.jpg';
 
 const FoodItems = [
 
--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -2,19 +2,52 @@ import React from 'react';
 
 import FoodItems from './FoodItems';
 
+import img1 from '../../img/personal/viola1.jpg';
+import img2 from '../../img/personal/viola2.jpg';
+
 const Personal = () => {
   return (
     <section data-index="5" id="personal">
       <h2>Personal</h2>
-      <h3>Music</h3>
-      <p>Text goes here about music</p>
 
-      <div id="cooking">
-        <div id="cookingText">
+      <div className="personalSection">
+        <div className="personalText">
+          <h3>🎻 Music</h3>
+          <p>Text goes here about music</p>
+        </div>
+
+        <div id="musicMedia">
+          <iframe
+            width="280"
+            height="160"
+            src="https://www.youtube.com/embed/tUo7kLDjtFQ"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+          <img src={img2} alt="default" />
+
+          <img src={img1} alt="default" />
+          <iframe
+            width="280"
+            height="160"
+            src="https://www.youtube.com/embed/MTrvcWXZhPQ"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      </div>
+
+      <div className="personalSection">
+        <div className="personalText">
           <h3>👨‍🍳 Cooking</h3>
           <p>I enjoy cooking delicious foods. Check out some of the
             dishes I&apos;ve made over the years!
           </p>
+
         </div>
 
         <div id="foodPictures">
