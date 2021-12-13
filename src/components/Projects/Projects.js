3b7871487b8fff052843e09b1d5bf9561753388e
JY2452 <jaeyoung.pak8@gmail.com>
2021-12-13 01:58:06 -0500

    mobile project view fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Fade from 'react-reveal/Fade';
+import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 import ProjectItems from './ProjectItems';
 
@@ -10,49 +10,50 @@ const Projects = () => {
 
       <div>
         {ProjectItems.map((item) => {
-          console.log(item);
           return (
-            <div className="projectItem" key={item.project}>
-              <div className="projectText">
+            <Fade bottom>
+              <div className="projectItem" key={item.project}>
+                <div className="projectText">
 
-                <p className="projectTitle">{item.project}</p>
+                  <p className="projectTitle">{item.project}</p>
 
-                <div className="projectLanguage">
-                  {item.languages.map((language) => {
-                    return (
-                      <p key={language.name}
-                        style={{ background: language.bg, color: language.color }}
-                      >
-                        {language.name}
-                      </p>
-                    );
-                  })}
+                  <div className="projectLanguage">
+                    {item.languages.map((language) => {
+                      return (
+                        <p key={language.name}
+                          style={{ background: language.bg, color: language.color }}
+                        >
+                          {language.name}
+                        </p>
+                      );
+                    })}
+                  </div>
+
+                  <p className="projectDescr">{item.description} {item.description2}</p>
+
+                  <div className="projectIcons">
+                    {item.icons.map((icon) => {
+                      return (
+                        <a href={icon.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          aria-label={item.name}
+                          key={icon.text}
+                        >
+                          <i className={icon.name}><p>{icon.text}</p></i>
+                        </a>
+                      );
+                    })}
+                  </div>
                 </div>
 
-                <p className="projectDescr">{item.description} {item.description2}</p>
-
-                <div className="projectIcons">
-                  {item.icons.map((icon) => {
-                    return (
-                      <a href={icon.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        aria-label={item.name}
-                        key={icon.text}
-                      >
-                        <i className={icon.name}><p>{icon.text}</p></i>
-                      </a>
-                    );
-                  })}
-                </div>
+                <Slide right>
+                  <div>
+                    <img className="projectImage" src={item.img.src} style={item.img.style} alt="ProjectImage" />
+                  </div>
+                </Slide>
               </div>
-
-              <Slide right>
-                <div>
-                  <img className="projectImage" src={item.img.src} style={item.img.style} alt="ProjectImage" />
-                </div>
-              </Slide>
-            </div>
+            </Fade>
 
           );
         })}
